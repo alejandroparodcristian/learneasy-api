81f8e6d7d38972c4136becefb968c4d40ca2bb1f
alejandroparodcristian <115542447+alejandroparodcristian@users.noreply.github.com>
2025-05-05 17:24:44 +0200

    registrar la fecha automaticamente

--- src/main/java/com/leaneasy/learneasyapi/Model/Usuario.java
+++ src/main/java/com/leaneasy/learneasyapi/Model/Usuario.java
@@ -5,7 +5,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+
 
 @Entity
 @Table(name = "usuario")
@@ -25,5 +26,5 @@ public class Usuario {
 
     private String password;
 
-    private LocalDateTime fechaRegistro;
+    private LocalDate fechaRegistro;
 }
--- src/main/java/com/leaneasy/learneasyapi/Service/UsuarioService.java
+++ src/main/java/com/leaneasy/learneasyapi/Service/UsuarioService.java
@@ -5,6 +5,7 @@ import com.leaneasy.learneasyapi.Repository.UsuarioRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -27,8 +28,16 @@ public class UsuarioService {
         if (!usuario.getPassword().startsWith("$2a$")) {
             usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
         }
+
+        // Solo asigna fecha si es un usuario nuevo
+        if (usuario.getId() == null && usuario.getFechaRegistro() == null) {
+            usuario.setFechaRegistro(LocalDate.now());
+        }
+
         return repo.save(usuario);
     }
+
+
 
     //Login busca por id y la contraseña
     public Usuario login(String email, String password) {
